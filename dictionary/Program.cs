58ad393fe5c3ef7f372589c65449a04fb754e5ab
Ameejac <ameejacherukuru123@gmail.com>
2021-12-16 23:13:22 +0530

    Ameeja[Added:To store And Remove The Elements in Dictionary]

---
+++ dictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DictionaryCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dictionary obj = new dictionary();
+            obj.AddElements();
+            
+        }
+    }
+}
---
+++ dictionary/dictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace DictionaryCode
+{
+    class dictionary
+    {
+	    //store the elements in Dictionary
+          public void AddElements()
+          {
+                Dictionary<int, string> list = new Dictionary<int, string>();
+                list.Add(1, "i ");
+                list.Add(2, "am");
+                list.Add(3, "a");
+                list.Add(4, "good");
+                list.Add(5, "person");
+                Console.WriteLine(" before remove:" + list.Count);
+                foreach (KeyValuePair<int, string> str in list)
+                {
+
+                    Console.WriteLine("{0} and {1}", str.Key, str.Value);
+                    // Console.WriteLine("inserting the elements: " + key + " " + value);
+                }
+		//remove method
+            list.Remove(1);
+            Console.WriteLine(" after remove:" + list.Count);
+            //remove multipul elements
+            int count = list.Count;
+                for (int i = 1; i < list.Count; i++)
+                {
+
+                    if (i < 3)
+                    {
+                        string ch = Convert.ToString(i);
+                        Console.WriteLine("remove elaents:" + i);
+                    }
+                }
+                Console.WriteLine("----->" + list.Count);
+                foreach (KeyValuePair<int, string> str in list)
+                {
+                    Console.WriteLine("{0} and {1}", str.Key, str.Value);
+                }
+          }
+     }
+}
+
+
